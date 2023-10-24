bb3115f0dfbc6509de72aa086c8fac49ab5e5068
haiduong12366 <130330980+haiduong12366@users.noreply.github.com>
2023-10-24 16:32:10 +0700

    haiduong

--- QLSV/Views/DangKySinhVien.Designer.cs
+++ QLSV/Views/DangKySinhVien.Designer.cs
@@ -40,22 +40,32 @@ namespace QLSV
             this.txtCCCD = new System.Windows.Forms.TextBox();
             this.txtHoTen = new System.Windows.Forms.TextBox();
             this.txtMaSv = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnDangKy
             // 
-            this.btnDangKy.Location = new System.Drawing.Point(449, 301);
+            this.btnDangKy.Location = new System.Drawing.Point(462, 273);
             this.btnDangKy.Margin = new System.Windows.Forms.Padding(2);
             this.btnDangKy.Name = "btnDangKy";
-            this.btnDangKy.Size = new System.Drawing.Size(168, 19);
+            this.btnDangKy.Size = new System.Drawing.Size(168, 49);
             this.btnDangKy.TabIndex = 27;
-            this.btnDangKy.Text = "button1";
+            this.btnDangKy.Text = "Dang Ky";
             this.btnDangKy.UseVisualStyleBackColor = true;
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(449, 230);
+            this.comboBox1.Location = new System.Drawing.Point(462, 202);
             this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(169, 21);
@@ -64,7 +74,7 @@ namespace QLSV
             // cbMaPhong
             // 
             this.cbMaPhong.FormattingEnabled = true;
-            this.cbMaPhong.Location = new System.Drawing.Point(449, 188);
+            this.cbMaPhong.Location = new System.Drawing.Point(462, 160);
             this.cbMaPhong.Margin = new System.Windows.Forms.Padding(2);
             this.cbMaPhong.Name = "cbMaPhong";
             this.cbMaPhong.Size = new System.Drawing.Size(169, 21);
@@ -73,7 +83,7 @@ namespace QLSV
             // cbMaToa
             // 
             this.cbMaToa.FormattingEnabled = true;
-            this.cbMaToa.Location = new System.Drawing.Point(449, 144);
+            this.cbMaToa.Location = new System.Drawing.Point(462, 116);
             this.cbMaToa.Margin = new System.Windows.Forms.Padding(2);
             this.cbMaToa.Name = "cbMaToa";
             this.cbMaToa.Size = new System.Drawing.Size(169, 21);
@@ -82,7 +92,7 @@ namespace QLSV
             // cbGioiTinh
             // 
             this.cbGioiTinh.FormattingEnabled = true;
-            this.cbGioiTinh.Location = new System.Drawing.Point(183, 230);
+            this.cbGioiTinh.Location = new System.Drawing.Point(142, 202);
             this.cbGioiTinh.Margin = new System.Windows.Forms.Padding(2);
             this.cbGioiTinh.Name = "cbGioiTinh";
             this.cbGioiTinh.Size = new System.Drawing.Size(169, 21);
@@ -90,7 +100,7 @@ namespace QLSV
             // 
             // dtpNgaySinh
             // 
-            this.dtpNgaySinh.Location = new System.Drawing.Point(183, 188);
+            this.dtpNgaySinh.Location = new System.Drawing.Point(142, 160);
             this.dtpNgaySinh.Margin = new System.Windows.Forms.Padding(2);
             this.dtpNgaySinh.Name = "dtpNgaySinh";
             this.dtpNgaySinh.Size = new System.Drawing.Size(169, 20);
@@ -98,7 +108,7 @@ namespace QLSV
             // 
             // txtDiaChi
             // 
-            this.txtDiaChi.Location = new System.Drawing.Point(183, 330);
+            this.txtDiaChi.Location = new System.Drawing.Point(142, 302);
             this.txtDiaChi.Margin = new System.Windows.Forms.Padding(2);
             this.txtDiaChi.Name = "txtDiaChi";
             this.txtDiaChi.Size = new System.Drawing.Size(169, 20);
@@ -106,7 +116,7 @@ namespace QLSV
             // 
             // txtSDT
             // 
-            this.txtSDT.Location = new System.Drawing.Point(449, 100);
+            this.txtSDT.Location = new System.Drawing.Point(462, 72);
             this.txtSDT.Margin = new System.Windows.Forms.Padding(2);
             this.txtSDT.Name = "txtSDT";
             this.txtSDT.Size = new System.Drawing.Size(169, 20);
@@ -114,7 +124,7 @@ namespace QLSV
             // 
             // txtCCCD
             // 
-            this.txtCCCD.Location = new System.Drawing.Point(183, 278);
+            this.txtCCCD.Location = new System.Drawing.Point(142, 250);
             this.txtCCCD.Margin = new System.Windows.Forms.Padding(2);
             this.txtCCCD.Name = "txtCCCD";
             this.txtCCCD.Size = new System.Drawing.Size(169, 20);
@@ -122,7 +132,7 @@ namespace QLSV
             // 
             // txtHoTen
             // 
-            this.txtHoTen.Location = new System.Drawing.Point(183, 144);
+            this.txtHoTen.Location = new System.Drawing.Point(142, 116);
             this.txtHoTen.Margin = new System.Windows.Forms.Padding(2);
             this.txtHoTen.Name = "txtHoTen";
             this.txtHoTen.Size = new System.Drawing.Size(169, 20);
@@ -130,17 +140,107 @@ namespace QLSV
             // 
             // txtMaSv
             // 
-            this.txtMaSv.Location = new System.Drawing.Point(183, 100);
+            this.txtMaSv.Location = new System.Drawing.Point(142, 72);
             this.txtMaSv.Margin = new System.Windows.Forms.Padding(2);
             this.txtMaSv.Name = "txtMaSv";
             this.txtMaSv.Size = new System.Drawing.Size(169, 20);
             this.txtMaSv.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 20);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Ma Sinh Vien";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 20);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "Ho ten";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 253);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 20);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "CCCD";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(12, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 20);
+            this.label4.TabIndex = 31;
+            this.label4.Text = "Gioi tinh";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(12, 166);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 20);
+            this.label5.TabIndex = 32;
+            this.label5.Text = "Ngay Sinh";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(12, 305);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 20);
+            this.label6.TabIndex = 33;
+            this.label6.Text = "Dia chi";
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(338, 166);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(103, 20);
+            this.label8.TabIndex = 38;
+            this.label8.Text = "Ma Phong";
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(338, 205);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(103, 20);
+            this.label9.TabIndex = 37;
+            this.label9.Text = "So ky";
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(338, 119);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(103, 20);
+            this.label11.TabIndex = 35;
+            this.label11.Text = "Ma toa";
+            // 
+            // label12
+            // 
+            this.label12.Location = new System.Drawing.Point(338, 72);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(103, 20);
+            this.label12.TabIndex = 34;
+            this.label12.Text = "SDT";
             // 
             // DangKySinhVien
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnDangKy);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.cbMaPhong);
@@ -172,6 +272,16 @@ namespace QLSV
         private System.Windows.Forms.TextBox txtCCCD;
         private System.Windows.Forms.TextBox txtHoTen;
         private System.Windows.Forms.TextBox txtMaSv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
     }
 }
 
