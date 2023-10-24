5af0811aac660dd0e551b1252334b7203fdfe8ee
haiduong12366 <130330980+haiduong12366@users.noreply.github.com>
2023-10-24 23:35:36 +0700

    form

--- QLSV/Views/DangKySinhVien.Designer.cs
+++ QLSV/Views/DangKySinhVien.Designer.cs
@@ -51,24 +51,30 @@ namespace QLSV
             this.label11 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnDangKy
             // 
-            this.btnDangKy.Location = new System.Drawing.Point(512, 275);
+            this.btnDangKy.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangKy.Location = new System.Drawing.Point(539, 256);
             this.btnDangKy.Margin = new System.Windows.Forms.Padding(2);
             this.btnDangKy.Name = "btnDangKy";
-            this.btnDangKy.Size = new System.Drawing.Size(182, 49);
+            this.btnDangKy.Size = new System.Drawing.Size(182, 46);
             this.btnDangKy.TabIndex = 27;
-            this.btnDangKy.Text = "Dang Ky";
+            this.btnDangKy.Text = "Đăng Ký";
             this.btnDangKy.UseVisualStyleBackColor = true;
             this.btnDangKy.Click += new System.EventHandler(this.btnDangKy_Click);
             // 
             // cbSoKy
             // 
             this.cbSoKy.FormattingEnabled = true;
-            this.cbSoKy.Location = new System.Drawing.Point(512, 204);
+            this.cbSoKy.Location = new System.Drawing.Point(539, 210);
             this.cbSoKy.Margin = new System.Windows.Forms.Padding(2);
             this.cbSoKy.Name = "cbSoKy";
             this.cbSoKy.Size = new System.Drawing.Size(182, 21);
@@ -77,7 +83,7 @@ namespace QLSV
             // cbMaPhong
             // 
             this.cbMaPhong.FormattingEnabled = true;
-            this.cbMaPhong.Location = new System.Drawing.Point(512, 162);
+            this.cbMaPhong.Location = new System.Drawing.Point(539, 168);
             this.cbMaPhong.Margin = new System.Windows.Forms.Padding(2);
             this.cbMaPhong.Name = "cbMaPhong";
             this.cbMaPhong.Size = new System.Drawing.Size(182, 21);
@@ -86,7 +92,7 @@ namespace QLSV
             // cbMaToa
             // 
             this.cbMaToa.FormattingEnabled = true;
-            this.cbMaToa.Location = new System.Drawing.Point(512, 118);
+            this.cbMaToa.Location = new System.Drawing.Point(539, 124);
             this.cbMaToa.Margin = new System.Windows.Forms.Padding(2);
             this.cbMaToa.Name = "cbMaToa";
             this.cbMaToa.Size = new System.Drawing.Size(182, 21);
@@ -96,7 +102,7 @@ namespace QLSV
             // cbGioiTinh
             // 
             this.cbGioiTinh.FormattingEnabled = true;
-            this.cbGioiTinh.Location = new System.Drawing.Point(192, 204);
+            this.cbGioiTinh.Location = new System.Drawing.Point(160, 167);
             this.cbGioiTinh.Margin = new System.Windows.Forms.Padding(2);
             this.cbGioiTinh.Name = "cbGioiTinh";
             this.cbGioiTinh.Size = new System.Drawing.Size(182, 21);
@@ -104,7 +110,7 @@ namespace QLSV
             // 
             // dtpkNgaySinh
             // 
-            this.dtpkNgaySinh.Location = new System.Drawing.Point(192, 162);
+            this.dtpkNgaySinh.Location = new System.Drawing.Point(160, 125);
             this.dtpkNgaySinh.Margin = new System.Windows.Forms.Padding(2);
             this.dtpkNgaySinh.Name = "dtpkNgaySinh";
             this.dtpkNgaySinh.Size = new System.Drawing.Size(182, 20);
@@ -112,7 +118,7 @@ namespace QLSV
             // 
             // txtDiaChi
             // 
-            this.txtDiaChi.Location = new System.Drawing.Point(192, 304);
+            this.txtDiaChi.Location = new System.Drawing.Point(160, 213);
             this.txtDiaChi.Margin = new System.Windows.Forms.Padding(2);
             this.txtDiaChi.Name = "txtDiaChi";
             this.txtDiaChi.Size = new System.Drawing.Size(182, 20);
@@ -120,7 +126,7 @@ namespace QLSV
             // 
             // txtSDT
             // 
-            this.txtSDT.Location = new System.Drawing.Point(512, 74);
+            this.txtSDT.Location = new System.Drawing.Point(539, 37);
             this.txtSDT.Margin = new System.Windows.Forms.Padding(2);
             this.txtSDT.Name = "txtSDT";
             this.txtSDT.Size = new System.Drawing.Size(182, 20);
@@ -129,7 +135,7 @@ namespace QLSV
             // 
             // txtCCCD
             // 
-            this.txtCCCD.Location = new System.Drawing.Point(192, 252);
+            this.txtCCCD.Location = new System.Drawing.Point(539, 84);
             this.txtCCCD.Margin = new System.Windows.Forms.Padding(2);
             this.txtCCCD.Name = "txtCCCD";
             this.txtCCCD.Size = new System.Drawing.Size(182, 20);
@@ -137,7 +143,7 @@ namespace QLSV
             // 
             // txtHoTen
             // 
-            this.txtHoTen.Location = new System.Drawing.Point(192, 118);
+            this.txtHoTen.Location = new System.Drawing.Point(160, 81);
             this.txtHoTen.Margin = new System.Windows.Forms.Padding(2);
             this.txtHoTen.Name = "txtHoTen";
             this.txtHoTen.Size = new System.Drawing.Size(182, 20);
@@ -145,7 +151,8 @@ namespace QLSV
             // 
             // txtMaSv
             // 
-            this.txtMaSv.Location = new System.Drawing.Point(192, 74);
+            this.txtMaSv.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtMaSv.Location = new System.Drawing.Point(160, 37);
             this.txtMaSv.Margin = new System.Windows.Forms.Padding(2);
             this.txtMaSv.Name = "txtMaSv";
             this.txtMaSv.Size = new System.Drawing.Size(182, 20);
@@ -153,111 +160,143 @@ namespace QLSV
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(62, 74);
+            this.label1.Location = new System.Drawing.Point(52, 37);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(103, 20);
             this.label1.TabIndex = 28;
-            this.label1.Text = "Ma Sinh Vien";
+            this.label1.Text = "Mã Sinh Viên:";
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(62, 121);
+            this.label2.Location = new System.Drawing.Point(52, 84);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(103, 20);
             this.label2.TabIndex = 29;
-            this.label2.Text = "Ho ten";
+            this.label2.Text = "Họ và Tên:";
             // 
             // label3
             // 
-            this.label3.Location = new System.Drawing.Point(62, 255);
+            this.label3.Location = new System.Drawing.Point(431, 87);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(103, 20);
             this.label3.TabIndex = 30;
-            this.label3.Text = "CCCD";
+            this.label3.Text = "CCCD:";
             // 
             // label4
             // 
-            this.label4.Location = new System.Drawing.Point(62, 207);
+            this.label4.Location = new System.Drawing.Point(52, 170);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(103, 20);
             this.label4.TabIndex = 31;
-            this.label4.Text = "Gioi tinh";
+            this.label4.Text = "Giới tính:";
             // 
             // label5
             // 
-            this.label5.Location = new System.Drawing.Point(62, 168);
+            this.label5.Location = new System.Drawing.Point(52, 131);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(103, 20);
             this.label5.TabIndex = 32;
-            this.label5.Text = "Ngay Sinh";
+            this.label5.Text = "Ngày Sinh:";
             // 
             // label6
             // 
-            this.label6.Location = new System.Drawing.Point(62, 307);
+            this.label6.Location = new System.Drawing.Point(52, 216);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(103, 20);
             this.label6.TabIndex = 33;
-            this.label6.Text = "Dia chi";
+            this.label6.Text = "Địa Chỉ";
             // 
             // label8
             // 
-            this.label8.Location = new System.Drawing.Point(404, 168);
+            this.label8.Location = new System.Drawing.Point(431, 174);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(103, 20);
             this.label8.TabIndex = 38;
-            this.label8.Text = "Ma Phong";
+            this.label8.Text = "Mã Phòng:";
             // 
             // label9
             // 
-            this.label9.Location = new System.Drawing.Point(404, 207);
+            this.label9.Location = new System.Drawing.Point(431, 213);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(103, 20);
             this.label9.TabIndex = 37;
-            this.label9.Text = "So ky";
+            this.label9.Text = "Số Kỳ Ở:";
             // 
             // label11
             // 
-            this.label11.Location = new System.Drawing.Point(404, 121);
+            this.label11.Location = new System.Drawing.Point(431, 127);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(103, 20);
             this.label11.TabIndex = 35;
-            this.label11.Text = "Ma toa";
+            this.label11.Text = "Mã Tòa:";
             // 
             // label12
             // 
-            this.label12.Location = new System.Drawing.Point(404, 74);
+            this.label12.Location = new System.Drawing.Point(431, 37);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(103, 20);
             this.label12.TabIndex = 34;
-            this.label12.Text = "SDT";
+            this.label12.Text = "Số Điện thoại:";
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.txtMaSv);
-            this.panel1.Controls.Add(this.label8);
-            this.panel1.Controls.Add(this.txtHoTen);
-            this.panel1.Controls.Add(this.label9);
-            this.panel1.Controls.Add(this.txtCCCD);
-            this.panel1.Controls.Add(this.label11);
-            this.panel1.Controls.Add(this.txtSDT);
-            this.panel1.Controls.Add(this.label12);
-            this.panel1.Controls.Add(this.txtDiaChi);
-            this.panel1.Controls.Add(this.label6);
-            this.panel1.Controls.Add(this.dtpkNgaySinh);
-            this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.cbGioiTinh);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.cbMaToa);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.cbMaPhong);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.cbSoKy);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.btnDangKy);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(776, 426);
             this.panel1.TabIndex = 39;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Yellow;
+            this.textBox1.Location = new System.Drawing.Point(2, 2);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(772, 61);
+            this.textBox1.TabIndex = 39;
+            this.textBox1.Text = "Đăng Ký Ở Ký Túc Xá Khu B";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Location = new System.Drawing.Point(6, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(770, 66);
+            this.panel2.TabIndex = 40;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel3.Controls.Add(this.txtMaSv);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.btnDangKy);
+            this.panel3.Controls.Add(this.cbSoKy);
+            this.panel3.Controls.Add(this.label8);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.txtHoTen);
+            this.panel3.Controls.Add(this.cbMaPhong);
+            this.panel3.Controls.Add(this.label9);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.txtCCCD);
+            this.panel3.Controls.Add(this.cbMaToa);
+            this.panel3.Controls.Add(this.label11);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.txtSDT);
+            this.panel3.Controls.Add(this.cbGioiTinh);
+            this.panel3.Controls.Add(this.label12);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.txtDiaChi);
+            this.panel3.Controls.Add(this.dtpkNgaySinh);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Location = new System.Drawing.Point(6, 75);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(765, 351);
+            this.panel3.TabIndex = 41;
             // 
             // DangKySinhVien
             // 
@@ -268,7 +307,10 @@ namespace QLSV
             this.Name = "DangKySinhVien";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -297,6 +339,9 @@ namespace QLSV
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
