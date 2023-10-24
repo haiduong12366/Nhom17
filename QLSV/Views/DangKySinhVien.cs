41873a1dfce58bf6481a86df3cbec6ee47ec4ca2
haiduong12366 <130330980+haiduong12366@users.noreply.github.com>
2023-10-24 15:37:55 +0700

    Haiduong

--- QLSV/Views/DangKySinhVien.cs
+++ QLSV/Views/DangKySinhVien.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace QLSV
 {
-    public partial class Form1 : Form
+    public partial class DangKySinhVien : Form
     {
-        public Form1()
+        public DangKySinhVien()
         {
             InitializeComponent();
         }
