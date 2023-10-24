a021081ad35cca83823ed13e3afc3644e319a4f6
haiduong12366 <130330980+haiduong12366@users.noreply.github.com>
2023-10-24 19:43:20 +0700

    dangky

---
+++ QLSV/DTO/Toa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSV.DTO
+{
+    class Toa
+    {
+        private string maToa;
+        private string tenToa;
+        private string gioiTinh;
+
+        public Toa(DataRow row)
+        {
+            this.MaToa = row["MaToa"].ToString();
+            this.TenToa = row["TenToa"].ToString();
+            this.GioiTinh = row["gioitinh"].ToString();
+
+        }
+
+        public string MaToa { get => maToa; set => maToa = value; }
+        public string TenToa { get => tenToa; set => tenToa = value; }
+        public string GioiTinh { get => gioiTinh; set => gioiTinh = value; }
+    }
+}
