799ce0d9b24b670df464eaff780326660293899c
qetuo46 <126058792+qetuo46@users.noreply.github.com>
2023-10-24 15:04:41 +0700

    DBConnection

---
+++ QLSV/DBConnection.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLSV
+{
+    internal class DBConnection
+    {
+        public SqlConnection conn = new SqlConnection(Properties.Settings.Default.cnnStr);
+        public void ThucThi(string sqlStr)
+        {
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sqlStr, conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Thuc thi that bai\n" + exc.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        public DataTable FormLoad(string sqlStr)
+        {
+            DataTable dataSet = new DataTable();
+            try
+            {
+                conn.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(sqlStr, conn);
+                adapter.Fill(dataSet);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+                return null;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dataSet;
+        }
+        public object GetItem(string sqlStr)
+        {
+            object result = new object();
+            try
+            {
+                conn.Open();
+                SqlCommand command = new SqlCommand(sqlStr, conn);
+                result = command.ExecuteScalar();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return result;
+        }
+    }
+}
