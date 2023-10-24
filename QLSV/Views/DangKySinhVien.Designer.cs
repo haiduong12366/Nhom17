b6b0a6ca7f0f08338526dd4318213f2f06ce6832
haiduong12366 <130330980+haiduong12366@users.noreply.github.com>
2023-10-24 16:23:07 +0700

    haiduong

--- QLSV/Views/DangKySinhVien.Designer.cs
+++ QLSV/Views/DangKySinhVien.Designer.cs
@@ -29,13 +29,149 @@ namespace QLSV
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnDangKy = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cbMaPhong = new System.Windows.Forms.ComboBox();
+            this.cbMaToa = new System.Windows.Forms.ComboBox();
+            this.cbGioiTinh = new System.Windows.Forms.ComboBox();
+            this.dtpNgaySinh = new System.Windows.Forms.DateTimePicker();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.txtSDT = new System.Windows.Forms.TextBox();
+            this.txtCCCD = new System.Windows.Forms.TextBox();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.txtMaSv = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDangKy
+            // 
+            this.btnDangKy.Location = new System.Drawing.Point(449, 301);
+            this.btnDangKy.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDangKy.Name = "btnDangKy";
+            this.btnDangKy.Size = new System.Drawing.Size(168, 19);
+            this.btnDangKy.TabIndex = 27;
+            this.btnDangKy.Text = "button1";
+            this.btnDangKy.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(449, 230);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(169, 21);
+            this.comboBox1.TabIndex = 26;
+            // 
+            // cbMaPhong
+            // 
+            this.cbMaPhong.FormattingEnabled = true;
+            this.cbMaPhong.Location = new System.Drawing.Point(449, 188);
+            this.cbMaPhong.Margin = new System.Windows.Forms.Padding(2);
+            this.cbMaPhong.Name = "cbMaPhong";
+            this.cbMaPhong.Size = new System.Drawing.Size(169, 21);
+            this.cbMaPhong.TabIndex = 25;
+            // 
+            // cbMaToa
+            // 
+            this.cbMaToa.FormattingEnabled = true;
+            this.cbMaToa.Location = new System.Drawing.Point(449, 144);
+            this.cbMaToa.Margin = new System.Windows.Forms.Padding(2);
+            this.cbMaToa.Name = "cbMaToa";
+            this.cbMaToa.Size = new System.Drawing.Size(169, 21);
+            this.cbMaToa.TabIndex = 24;
+            // 
+            // cbGioiTinh
+            // 
+            this.cbGioiTinh.FormattingEnabled = true;
+            this.cbGioiTinh.Location = new System.Drawing.Point(183, 230);
+            this.cbGioiTinh.Margin = new System.Windows.Forms.Padding(2);
+            this.cbGioiTinh.Name = "cbGioiTinh";
+            this.cbGioiTinh.Size = new System.Drawing.Size(169, 21);
+            this.cbGioiTinh.TabIndex = 23;
+            // 
+            // dtpNgaySinh
+            // 
+            this.dtpNgaySinh.Location = new System.Drawing.Point(183, 188);
+            this.dtpNgaySinh.Margin = new System.Windows.Forms.Padding(2);
+            this.dtpNgaySinh.Name = "dtpNgaySinh";
+            this.dtpNgaySinh.Size = new System.Drawing.Size(169, 20);
+            this.dtpNgaySinh.TabIndex = 22;
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Location = new System.Drawing.Point(183, 330);
+            this.txtDiaChi.Margin = new System.Windows.Forms.Padding(2);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(169, 20);
+            this.txtDiaChi.TabIndex = 21;
+            // 
+            // txtSDT
+            // 
+            this.txtSDT.Location = new System.Drawing.Point(449, 100);
+            this.txtSDT.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSDT.Name = "txtSDT";
+            this.txtSDT.Size = new System.Drawing.Size(169, 20);
+            this.txtSDT.TabIndex = 20;
+            // 
+            // txtCCCD
+            // 
+            this.txtCCCD.Location = new System.Drawing.Point(183, 278);
+            this.txtCCCD.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCCCD.Name = "txtCCCD";
+            this.txtCCCD.Size = new System.Drawing.Size(169, 20);
+            this.txtCCCD.TabIndex = 19;
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(183, 144);
+            this.txtHoTen.Margin = new System.Windows.Forms.Padding(2);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(169, 20);
+            this.txtHoTen.TabIndex = 18;
+            // 
+            // txtMaSv
+            // 
+            this.txtMaSv.Location = new System.Drawing.Point(183, 100);
+            this.txtMaSv.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMaSv.Name = "txtMaSv";
+            this.txtMaSv.Size = new System.Drawing.Size(169, 20);
+            this.txtMaSv.TabIndex = 17;
+            // 
+            // DangKySinhVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDangKy);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cbMaPhong);
+            this.Controls.Add(this.cbMaToa);
+            this.Controls.Add(this.cbGioiTinh);
+            this.Controls.Add(this.dtpNgaySinh);
+            this.Controls.Add(this.txtDiaChi);
+            this.Controls.Add(this.txtSDT);
+            this.Controls.Add(this.txtCCCD);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.txtMaSv);
+            this.Name = "DangKySinhVien";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnDangKy;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cbMaPhong;
+        private System.Windows.Forms.ComboBox cbMaToa;
+        private System.Windows.Forms.ComboBox cbGioiTinh;
+        private System.Windows.Forms.DateTimePicker dtpNgaySinh;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.TextBox txtSDT;
+        private System.Windows.Forms.TextBox txtCCCD;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.TextBox txtMaSv;
     }
 }
 
